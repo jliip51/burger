69ce3f0a7fbf56b7f37883f60677ff94979d4c77
jliip51 <jliip51@gmail.com>
2017-08-16 23:32:30 -0400

    fix validation msg

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -3,18 +3,24 @@ var burgers = require('../models/burger.js');
 
 var router = express.Router();
 
+ function getSum(data){
+  var eatenArray = [];
+  var sum;
+  for (i = 0; i < data.length; i++) {
+    eatenArray.push(data[i].count_eaten);
+  }
+  function sumArr(total, num) {
+  return total + num;
+  };
+  sum = eatenArray.reduce(sumArr);
+  return sum;
+};
+
 router.get("/", function(req, res) {
   burgers.all(function(data) {
-    var eatenArray = [];
-    for (i = 0; i < data.length; i++) {
-      eatenArray.push(data[i].count_eaten);
-    }
-    function sumArr(total, num) {
-    return total + num;
-    };
     var hbsObject = {
       burger: data,
-      total: eatenArray.reduce(sumArr)
+      total: getSum(data)
     };
     console.log(hbsObject);
     res.render("index", hbsObject);
@@ -22,15 +28,28 @@ router.get("/", function(req, res) {
 });
 
 router.post("/add", function(req, res) {
-  console.log(req.body.name);
-  burgers.create(req.body.name, function(resp) {
-    console.log(resp);
-    res.redirect("/");
+  console.log("New Name Test:" + req.body.name +":Test");
+  console.log(req.body.name.length)
+  if (req.body.name.length > 0 && req.body.name !== " ")  {
+    console.log(req.body.name);
+    burgers.create(req.body.name, function(resp) {
+      console.log(resp);
+      return res.redirect("/");
+    });
+  } else {
+  burgers.all(function(data) {
+    var hbsObject = {
+      burger: data,
+      total: getSum(data),
+      alert: 'Input Name Before Clicking Submit Button.'
+    };
+    console.log(hbsObject);
+    res.render("index", hbsObject);
   });
+}
 });
 
 router.put("/:id", function(req, res) {
-  // var condition = "id = " + req.params.id;
   burgers.update(req.params.id, req.body.devoured, function(result) {
   console.log(result);
   res.redirect("/");
